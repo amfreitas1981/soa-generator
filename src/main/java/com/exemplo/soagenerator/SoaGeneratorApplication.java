fea0f2e21e542df121c9335f2811a42fe5f8acaf
Alexandre Freitas <alexandremfreitas@gmail.com>
2025-02-02 17:33:15 -0300

    Versao Inicial

---
+++ src/main/java/com/exemplo/soagenerator/SoaGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.soagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemplo/soagenerator/SoaGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.soagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
